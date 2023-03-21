8b67a7635f8167c33df72fdfe1e3cb06cf9ae9b8
AlexEatDonut <alexeatdonut@gmail.com>
2023-03-21 08:41:01 +0100

    testing movement

--- assets/script/script.js
+++ assets/script/script.js
@@ -53,13 +53,6 @@ function accelerationHandler(acceleration, targetId) {
   let x = acceleration.x && acceleration.x.toFixed(0);
   let y = acceleration.y && acceleration.y.toFixed(0);
   let z = acceleration.z && acceleration.z.toFixed(0);
-  info = xyz.replace("X", x);
-  info = info.replace("Y", y);
-  info = info.replace("Z", z);
-  // info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  // info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  // info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
 
   var ball = $(".ball");
 
@@ -75,6 +68,14 @@ function accelerationHandler(acceleration, targetId) {
     right: _pos.x,
     top: _pos.y,
   });
+
+  info = xyz.replace("X", _pos.x);
+  info = info.replace("Y", _pos.y);
+  info = info.replace("Z", z);
+  // info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
+  // info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
+  // info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
+  document.getElementById(targetId).innerHTML = info;
 }
 
 function rotationHandler(rotation) {
