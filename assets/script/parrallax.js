c548d2a69cd742bb2216103d73d11c9aed730fbe
AlexEatDonut <alexeatdonut@gmail.com>
2023-03-22 14:58:04 +0100

    testing for mobile

--- assets/script/parrallax.js
+++ assets/script/parrallax.js
@@ -1,71 +1,10 @@
-let $layer_0 = $(".layer-0"),
-  $layer_1 = $(".layer-1"),
-  $layer_2 = $(".layer-2"),
-  $x_axis = $("#x-axis"),
-  $y_axis = $("#y-axis"),
-  $container = $("body"),
-  container_w = $container.width(),
-  container_h = $container.height();
-
-$(window).on("mousemove.parallax", function (event) {
-  if ($(window).width() < 580) {
-    left = 0;
-    top = 0;
-
-    left = container_w / 2 - s_pos_x;
-    top = container_h / 2 - s_pos_y;
-    console.log(s_pos_x, s_pos_y);
-  } else {
-  }
-  let p_pos_x = event.pageX,
-    p_pos_y = event.pageY,
-    left = 0,
-    top = 0;
-
-  left = container_w / 2 - p_pos_x;
-  top = container_h / 2 - p_pos_y;
-  console.log(p_pos_x, p_pos_y);
-  TweenMax.to($x_axis, 1, {
-    css: {
-      transform: "translateX(" + left * -1 + "px)",
-    },
-    ease: Expo.easeOut,
-    overwrite: "all",
-  });
-
-  TweenMax.to($y_axis, 1, {
-    css: {
-      transform: "translateY(" + top * -1 + "px)",
-    },
-    ease: Expo.easeOut,
-    overwrite: "all",
-  });
-
-  TweenMax.to($layer_2, 1, {
-    css: {
-      transform:
-        "translateX(" + left / 12 + "px) translateY(" + top / 6 + "px)",
-    },
-    ease: Expo.easeOut,
-    overwrite: "all",
-  });
-
-  TweenMax.to($layer_1, 1, {
-    css: {
-      transform: "translateX(" + left / 8 + "px) translateY(" + top / 4 + "px)",
-    },
-    ease: Expo.easeOut,
-    overwrite: "all",
-  });
-
-  TweenMax.to($layer_0, 10, {
-    css: {
-      transform: "rotate(" + left / 200 + "deg)",
-    },
-    ease: Expo.easeOut,
-    overwrite: "none",
-  });
+jQuery.extend({
+  percentage: function (a, b) {
+    return Math.round((a / b) * 100);
+  },
 });
+
+// TESTING TO SEE IF THERE IS A GYROSCOPE
 
 if ("LinearAccelerationSensor" in window && "Gyroscope" in window) {
   document.getElementById("moApi").innerHTML = "Generic Sensor API";
@@ -116,20 +55,93 @@ if ("LinearAccelerationSensor" in window && "Gyroscope" in window) {
     "No Accelerometer & Gyroscope API available";
 }
 
+//DEFINITION VARIABLES
+
+let $layer_0 = $(".layer-0"),
+  $layer_1 = $(".layer-1"),
+  $layer_2 = $(".layer-2"),
+  $x_axis = $("#x-axis"),
+  $y_axis = $("#y-axis"),
+  $ball = $(".ball"),
+  $container = $("body"),
+  container_w = $container.width(),
+  container_h = $container.height(),
+  p_left = 0,
+  p_top = 0;
+
+let winWidth = $(window).width();
+let winHeight = $(window).height();
+
+alert($.percentage(100, winWidth));
+
+// let s_pos_x = winWidth / (100 / toPercentage(x, 1));
+// let s_pos_y = winHeight / (100 / toPercentage(y, 1));
+
+let s_pos_x = $.percentage(100, winWidth);
+let s_pos_y = $.percentage(100, winWidth);
+
+$(window).on("mousemove.parallax", function (event) {
+  if ($(window).width() < 580) {
+    p_left = container_w / 2 - s_pos_x;
+    p_top = container_h / 2 - s_pos_y;
+    console.log(s_pos_x, s_pos_y);
+  } else {
+  }
+  let p_pos_x = event.pageX,
+    p_pos_y = event.pageY,
+    p_left = container_w / 2 - p_pos_x;
+  p_top = container_h / 2 - p_pos_y;
+  console.log(p_pos_x, p_pos_y);
+  TweenMax.to($x_axis, 1, {
+    css: {
+      transform: "translateX(" + p_left * -1 + "px)",
+    },
+    ease: Expo.easeOut,
+    overwrite: "all",
+  });
+
+  TweenMax.to($y_axis, 1, {
+    css: {
+      transform: "translateY(" + p_top * -1 + "px)",
+    },
+    ease: Expo.easeOut,
+    overwrite: "all",
+  });
+
+  TweenMax.to($layer_2, 1, {
+    css: {
+      transform:
+        "translateX(" + p_left / 12 + "px) translateY(" + p_top / 6 + "px)",
+    },
+    ease: Expo.easeOut,
+    overwrite: "all",
+  });
+
+  TweenMax.to($layer_1, 1, {
+    css: {
+      transform:
+        "translateX(" + p_left / 8 + "px) translateY(" + p_top / 4 + "px)",
+    },
+    ease: Expo.easeOut,
+    overwrite: "all",
+  });
+
+  TweenMax.to($layer_0, 10, {
+    css: {
+      transform: "rotate(" + p_left / 200 + "deg)",
+    },
+    ease: Expo.easeOut,
+    overwrite: "none",
+  });
+});
+
 function accelerationHandler(acceleration, targetId) {
   var info,
     xyz = "[X, Y, Z]";
-  let x = acceleration.x && acceleration.x.toFixed(0);
-  let y = acceleration.y && acceleration.y.toFixed(0);
-  let z = acceleration.z && acceleration.z.toFixed(0);
+  x = acceleration.x && acceleration.x.toFixed(0);
+  y = acceleration.y && acceleration.y.toFixed(0);
+  z = acceleration.z && acceleration.z.toFixed(0);
 
-  let ball = $(".ball");
-
-  let winWidth = $(window).width();
-  let winHeight = $(window).height();
-
-  let s_pos_x = winWidth / (100 / toPercentage(x, 1));
-  let s_pos_y = winHeight / (100 / toPercentage(y, 1));
   console.log(s_pos_x, s_pos_y);
   TweenMax.to(ball, 1, {
     css: {
