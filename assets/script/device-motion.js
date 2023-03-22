6a590b77bafc6603b548d74ac4affc169ad2ff13
AlexEatDonut <alexeatdonut@gmail.com>
2023-03-22 14:20:39 +0100

    code changes testing

--- assets/script/device-motion.js
+++ assets/script/device-motion.js
@@ -59,21 +59,19 @@ function accelerationHandler(acceleration, targetId) {
   let winWidth = $(window).width();
   let winHeight = $(window).height();
 
-  let s_pos = {
-    x: winWidth / (100 / toPercentage(x, 1)),
-    y: winHeight / (100 / toPercentage(y, 1)),
-  };
+  let s_pos_x = winWidth / (100 / toPercentage(x, 1));
+  let s_pos_y = winHeight / (100 / toPercentage(y, 1));
 
   TweenMax.to(ball, 1, {
     css: {
-      right: s_pos.x,
-      top: s_pos.y,
+      right: s_pos_x,
+      top: s_pos_y,
     },
   }),
     //   (info = xyz.replace("X", s_pos.x));
     // info = info.replace("Y", s_pos.y);
     // info = info.replace("Z", z);
-    console.log(s_pos.x, x_pos.y);
+    console.log(s_pos_x, x_pos_y);
   info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
   info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
   info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
